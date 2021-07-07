80392791a875fdd6ab1a2e01b632cc568a24a2c4
lerisse <tristanlerisse@gmail.com>
2021-07-07 12:40:46 -0400

    Building Radio app local first

--- client/src/Radio.js
+++ client/src/Radio.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { RadioBrowserApi } from "radio-browser-api";
 
-export default function Radio(props) {
+export default function Radio() {
+
+
+  const api = new RadioBrowserApi("MVP Radio App");
+
+    // query stations by languge and tag
+// const stations = await api.searchStations({
+//   language: 'english',
+//   tag:'jazz'
+//   limit: 100
+// })
+
   return (
-    <div>
-   <p>Radio goes here</p>
+    <div className="radio">
+      <h2>Radio goes here</h2>
     </div>
   );
 }
