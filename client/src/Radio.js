c3587805ba2a0a741e68df9b8e21d9e9a8513d95
lerisse <tristanlerisse@gmail.com>
2021-07-07 12:58:04 -0400

    Got response from RadioBrowserAPI

--- client/src/Radio.js
+++ client/src/Radio.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React , {useState,useEffect} from 'react';
 import { RadioBrowserApi } from "radio-browser-api";
 
 export default function Radio() {
+  const [stations, setStations] = useState();
 
+  const getStations = async () => {
 
   const api = new RadioBrowserApi("MVP Radio App");
 
-    // query stations by languge and tag
-// const stations = await api.searchStations({
-//   language: 'english',
-//   tag:'jazz'
-//   limit: 100
-// })
+  // query stations by languge and tag
+  const stations = await api.searchStations({
+    language: 'english',
+    tag:'jazz',
+    limit: 10
+  }).then(response => {
+    setStations(response);
+  })
+  return stations;
+}
+
+  useEffect(() => {
+    getStations();
+  });
 
   return (
     <div className="radio">
-      <h2>Radio goes here</h2>
+      <div className="stations">{stations}</div>
     </div>
   );
 }
